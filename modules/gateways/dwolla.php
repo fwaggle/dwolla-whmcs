5046e6185d3efc6d95a8246b505d97a8bb630404
Jamie Fraser <fwagglechop@gmail.com>
2014-03-26 22:40:41 +1100

    Form generation should be complete
    No callbacks yet, and untested.

--- modules/gateways/dwolla.php
+++ modules/gateways/dwolla.php
@@ -2,16 +2,48 @@
 ## WHMCS Gateway module for Dwolla
 ## Copyright (c) 2014, Sabrienix Communications
 
+ini_set("display_errors", 1);
+
 function dwolla_config() {
 	$configarray = array(
 		"FriendlyName" => array("Type" => "System", "Value"=>"Dwolla"),
-		"apiKey" => array('FriendlyName' => 'API Key from your bitpay.com account.', 'Type' => 'text'),
+		"dwollaId" => array("FriendlyName" => "Dwolla ID for your account.", "Type" => "text"),
+		"apiKey" => array("FriendlyName" => "API Key from your Dwolla account.", "Type" => "text"),
+		"apiSecret" => array("FriendlyName" => "API Secret from your Dwolla account.", "Type" => "text"),
 	);
 	
 	return $configarray;
 }
 
 function dwolla_link($params) {
-	$Dwolla = new DwollaRestClient($apiKey, $apiSecret, 'http://localhost:8888/offsiteGateway.php');
+	$timestamp = time();
+	
+	$post = array(
+		'key' => $params['apiKey'],
+		'signature' => hash_hmac('sha1', "{$params['apiKey']}&{$timestamp}&{$params['invoiceid']}", $params['apiSecret']),
+		'timestamp' => $timestamp,
+		'callback' => $params['systemurl'].'/modules/gateways/callback/dwolla.php',
+		'redirect' => $params['systemurl'].'/viewinvoice.php?id='.$params['invoiceid'],
+		'orderId' => $params['invoiceid'],
+		'destinationId' => $params['dwollaId'],
+		'amount' => $params['amount'],
+		'shipping' => 0,
+		'tax' => 0,
+		'name' => 'Invoice #' . $params['invoiceid'],
+		'description' => $params['description'],
+		
+		# Uncomment this to test the module.
+		'test' => 'true'
+	);
+	
+	$form = '<form accept-charset="UTF-8"  action="https://www.dwolla.com/payment/pay" method="post">';
+	
+	foreach($post as $key => $value)
+		$form .= '<input type="hidden" name="' . $key . '" value="' . $value . '" />';
+	
+	$form .= '<button type="submit">Pay with Dwolla</button>';
+	$form .= '</form>';
+	
+	return $form;
 }
 ?>
